0e98b5fb96dc883b144542697830c1179d91b4e4
githubinc-cyber <94745103+yalcin-mrl@users.noreply.github.com>
2021-11-23 22:03:23 +0300

    Not ortalama

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String []args){
+        Scanner nots = new Scanner(System.in);
+        double matNot,fizNot,kimNot,turNot,tarihNot,muzNot;
+        System.out.println("Matematik notu gir: ");
+        matNot = nots.nextDouble();
+        System.out.println("fizik notu gir: ");
+        fizNot = nots.nextDouble();
+        System.out.println("Kimya notu gir: ");
+        kimNot = nots.nextDouble();
+        System.out.println("Turkce notu gir: ");
+        turNot = nots.nextDouble();
+        System.out.println("Tarih notu gir: ");
+        tarihNot = nots.nextDouble();
+        System.out.println("Muzik notu gir: ");
+        muzNot = nots.nextDouble();
+        double notOrt = (matNot+fizNot+kimNot+turNot+tarihNot+muzNot)/6;
+        if(notOrt>=60){
+            System.out.println("Geçti");
+        }
+        else{
+            System.out.println("Kaldı");
+        }
+    }
+}
